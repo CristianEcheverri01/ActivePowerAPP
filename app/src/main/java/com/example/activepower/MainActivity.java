23d7dea82cdf8b4b376681216576d795decc3018
Cristian <cecheverrigallego@gmail.com>
2020-10-29 22:00:28 +0000

    creacion de menu inferior(menu tabs)

--- app/src/main/java/com/example/activepower/MainActivity.java
+++ app/src/main/java/com/example/activepower/MainActivity.java
@@ -1,16 +1,55 @@
 package com.example.activepower;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.activepower.fragments.HomeFragment;
+import com.example.activepower.fragments.ProfileFragment;
+import com.example.activepower.fragments.SettingFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
+    BottomNavigationView mBottonNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //show default fragment
+        showSelectFragment(new HomeFragment());
+
+        mBottonNavigationView = (BottomNavigationView) findViewById(R.id.nav);
+
+        mBottonNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                if(item.getItemId() == R.id.menu_profile){
+                    showSelectFragment(new ProfileFragment());
+                }
+                if(item.getItemId() == R.id.menu_home){
+                    showSelectFragment(new HomeFragment());
+                }
+                if(item.getItemId() == R.id.menu_settings){
+                    showSelectFragment(new SettingFragment());
+                }
+
+                return true;
+            }
+        });
     }
 
-    int i = 45;
+    /* metodo para seleccionar un fragmento */
+
+    private void showSelectFragment(Fragment f){
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, f)
+                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                .commit();
+    }
 }
